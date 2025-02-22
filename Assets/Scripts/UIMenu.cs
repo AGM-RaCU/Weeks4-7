c08d4d090e1f6785279a3a675696dba582cb3602
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-02-21 20:02:40 -0500

    Slider works now

--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -10,10 +10,18 @@ public class UIMenu : MonoBehaviour
     public TMP_Dropdown dropdown;
     public SpriteRenderer CoinSprite;
     public GameObject CoinsPrefab;
+    public float TossForce = 0.0f;
+    public Slider slider;
 
     private void Start()
     {
         CoinSprite.sprite = dropdown.options[0].image;
+       
+    }
+
+    public void OnSliderValueChanged(float TossForce)
+    {
+        Debug.Log(TossForce);
     }
 
     public void OnValueChanged(int index)
