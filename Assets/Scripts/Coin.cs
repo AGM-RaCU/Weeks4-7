0e3ad04b8ae170b08779c803c216b0648b70f366
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-02-21 21:47:47 -0500

    Final Commit, all done, added comments after merging to main

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,27 +6,25 @@ using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
-    public SpriteRenderer CSpriteRenderer;
-    public float DespawnCount;
-    public float DespawnLimit;
+    public SpriteRenderer CSpriteRenderer;//sprite renderer
+    public float DespawnCount;//timer to despawn prefab
+    public float DespawnLimit;//time limit to despawn prefab
 
-    // Start is called before the first frame update
     void Start()
     {
-        DespawnLimit = Random.Range(1, 10);
-        Vector2 Position = transform.position;
-        Position.x = Random.Range(-10, 10);
-        transform.position = Position;
+        DespawnLimit = Random.Range(1, 10);//randomizer for time limit
+        Vector2 Position = transform.position;//position variable to change prefab spawn location
+        Position.x = Random.Range(-10, 10);//x position randomizer
+        transform.position = Position;//position transform
     }
 
-    // Update is called once per frame
     void Update()
     {
-        DespawnCount += Time.deltaTime;
-        Debug.Log(DespawnCount);
-        if(DespawnCount >= DespawnLimit)
+        DespawnCount += Time.deltaTime;//increase timer as time passes
+        Debug.Log(DespawnCount);//check timer on console
+        if(DespawnCount >= DespawnLimit)//check if timer reached time limit
         {
-            Destroy(gameObject);
+            Destroy(gameObject);//destroy prefab when time limit is reached
         }
        
     }
--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -7,47 +7,47 @@ using UnityEngine.UI;
 public class UIMenu : MonoBehaviour
 {
 
-    public TMP_Dropdown dropdown;
-    public SpriteRenderer CoinSprite;
-    public GameObject CoinsPrefab;
-    public float TossForce = 0.0f;
-    public Slider slider;
+    public TMP_Dropdown dropdown;//dropdown
+    public SpriteRenderer CoinSprite;//sprite renderer for coin sprite
+    public GameObject CoinsPrefab;//coin prefab
+    public float TossForce = 0.0f;// how hard coin will be tossed
+    public Slider slider;//toss force slider
 
     private void Start()
     {
-        CoinSprite.sprite = dropdown.options[0].image;
+        CoinSprite.sprite = dropdown.options[0].image;//start on loonie
        
     }
 
     public void OnSliderValueChanged(float TossForce)
     {
-        Debug.Log(TossForce);
+        Debug.Log(TossForce);//check for toss force on console
 
-        Vector2 Position = CoinsPrefab.transform.position;
+        Vector2 Position = CoinsPrefab.transform.position;//position variable to change prefab spawn position
 
-        if (TossForce == 0.0f)
+        if (TossForce == 0.0f)//check toss force for 0 force
         {
-            Position.y = 1;
+            Position.y = 1;//spawn near bottom of the screen
         }
-        if (TossForce == 1.0f)
+        if (TossForce == 1.0f)//check toss force for 1 force
         {
-            Position.y = 3;
+            Position.y = 3;//spawn near middle of the screen
         }
-        if (TossForce == 2.0f)
+        if (TossForce == 2.0f)//check toss force for 2 force
         { 
-            Position.y = 5;
+            Position.y = 5;//spawn near top of the screen
         }
 
     }
 
     public void OnValueChanged(int index)
     {
-        CoinSprite.sprite = dropdown.options[index].image;
+        CoinSprite.sprite = dropdown.options[index].image;//change sprite in dropdown
     }
 
     public void spawn()
     {
-        GameObject newCoin = Instantiate(CoinsPrefab);
-        newCoin.GetComponent<Coin>().CSpriteRenderer.sprite = CoinSprite.sprite;
+        GameObject newCoin = Instantiate(CoinsPrefab);//get coin prefab
+        newCoin.GetComponent<Coin>().CSpriteRenderer.sprite = CoinSprite.sprite;//apply coin sprite to prefab
     }
 }
