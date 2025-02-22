7cf83987e9a0bfd2d6c9c262c02414abe22d9781
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-02-21 20:36:58 -0500

    Everything except Y position and prefab destruction has been implemented

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -7,16 +7,19 @@ using UnityEngine.UI;
 public class Coin : MonoBehaviour
 {
     public SpriteRenderer CSpriteRenderer;
-    
+    public float Timer;
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        Vector2 Position = transform.position;
+        Position.x = Random.Range(-10, 10);
+        transform.position = Position;
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        Timer += Time.deltaTime;
     }
 }
